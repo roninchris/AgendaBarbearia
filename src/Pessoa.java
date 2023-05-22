bf79da7b00c64d208d33ae8d6b6f66120b3e47bb
Sidnei Henrique <sidneii.henrique@hotmail.com>
2023-05-22 11:32:18 -0300

    Classe Pessoa criada

---
+++ src/Pessoa.java
@@ -0,0 +1,13 @@
+public class Pessoa {
+    private String nome;
+    private int telefone;
+    private String email;
+    private String senha;
+
+    public Pessoa(String nome, int telefone, String email, String senha) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.senha = senha;
+    }
+}
