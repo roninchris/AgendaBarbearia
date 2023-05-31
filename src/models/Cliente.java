dfbaba3e112cf979a04d1238007cc1cd3475aca6
João Victor Dos Santos Ferreira <joaovictor2111@gmail.com>
2023-05-31 10:36:55 -0300

    feat: remover clientes

--- src/models/Cliente.java
+++ src/models/Cliente.java
@@ -2,11 +2,19 @@ package models;
 
 import java.util.ArrayList;
 
-public class Cliente extends Pessoa{
-    private ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+public class Cliente extends Pessoa {
+    private static ArrayList<Cliente> clientes = new ArrayList<>();
+
     public Cliente(String nome, String email, String senha, String telefone) {
         super(nome, email, senha, telefone);
-
         clientes.add(this);
+    }
+
+    public static void excluirCliente(Cliente cliente) {
+        clientes.remove(cliente);
+    }
+
+    public static ArrayList<Cliente> getClientes() {
+        return clientes;
     }
 }
