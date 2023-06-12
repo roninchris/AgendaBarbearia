5c8861a6ede4883327388bba1bcaa0a92cf74a7a
Sidnei Henrique <henrisrs@hotmail.com>
2023-06-11 21:21:13 -0300

    Agora na hora de criar os horários dos barbeiros, existe uma verificação se o dia do mês já não passou

--- src/models/Agenda.java
+++ src/models/Agenda.java
@@ -3,9 +3,8 @@ package models;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.ArrayList;
-import java.util.Date;
+
 
 public class Agenda {
     private Barbeiro barbeiro;
@@ -20,22 +19,43 @@ public class Agenda {
         ArrayList<Horario> horarios = new ArrayList<>();
 
         // Obtém a data atual
-        LocalDate dataAtual = LocalDate.now().plusDays(1); // Adiciona um dia
+        LocalDate dataAtual = LocalDate.now();
+
+        // Obtém o primeiro dia do mês atual
+        LocalDate primeiroDiaMesAtual = dataAtual.withDayOfMonth(1);
 
         // Define o horário de início e fim
         LocalTime horarioInicio = LocalTime.of(8, 0);
         LocalTime horarioFim = LocalTime.of(19, 0);
 
-        // Cria os horários com intervalo de 1 hora
-        LocalDateTime dataHora = LocalDateTime.of(dataAtual, horarioInicio);
-        while (dataHora.toLocalTime().isBefore(horarioFim)) {
-            Horario horario = new Horario(dataHora, true, barbeiro);
-            horarios.add(horario);
-            dataHora = dataHora.plusHours(1);
+        // Verifica se a data atual é igual ou posterior ao primeiro dia do mês atual
+        if (dataAtual.isAfter(primeiroDiaMesAtual) || dataAtual.isEqual(primeiroDiaMesAtual)) {
+            // Cria os horários para o dia atual com intervalo de 1 hora
+            LocalDateTime dataHora = LocalDateTime.of(dataAtual, horarioInicio);
+            while (dataHora.toLocalTime().isBefore(horarioFim)) {
+                Horario horario = new Horario(dataHora, true, barbeiro);
+                horarios.add(horario);
+                dataHora = dataHora.plusHours(1);
+            }
+        }
+
+        // Percorre os dias restantes do mês atual
+        LocalDate data = primeiroDiaMesAtual.plusDays(1);
+        while (data.getMonthValue() == primeiroDiaMesAtual.getMonthValue()) {
+            // Cria os horários para cada dia com intervalo de 1 hora
+            LocalDateTime dataHora = LocalDateTime.of(data, horarioInicio);
+            while (dataHora.toLocalTime().isBefore(horarioFim)) {
+                Horario horario = new Horario(dataHora, true, barbeiro);
+                horarios.add(horario);
+                dataHora = dataHora.plusHours(1);
+            }
+            data = data.plusDays(1);
         }
 
         return horarios;
     }
+
+
     public ArrayList<Horario> getHorarios() {
         return horarios;
     }
